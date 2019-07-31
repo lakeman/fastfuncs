a221f9682e26ee6a4296a21040c07f587bc602ef
Nicolas GEORGES <nicolas54.georges@gmail.com>
2019-07-31 13:21:34 +0100

    add pb2019 target

--- libs/build_libs.bat
+++ libs/build_libs.bat
@@ -7,6 +7,7 @@ lib /MACHINE:X86 /DEF:pbshr115.def /OUT:pbshr115.lib
 lib /MACHINE:X86 /DEF:pbshr120.def /OUT:pbshr120.lib
 lib /MACHINE:X86 /DEF:pbshr125.def /OUT:pbshr125.lib
 lib /MACHINE:X86 /DEF:pbshr170.def /OUT:pbshr170.lib
+lib /MACHINE:X86 /DEF:pbshr190.def /OUT:pbshr190.lib
 
 lib /MACHINE:X86 /DEF:pbvm100.def /OUT:pbvm100.lib
 lib /MACHINE:X86 /DEF:pbvm105.def /OUT:pbvm105.lib
@@ -15,3 +16,4 @@ lib /MACHINE:X86 /DEF:pbvm115.def /OUT:pbvm115.lib
 lib /MACHINE:X86 /DEF:pbvm120.def /OUT:pbvm120.lib
 lib /MACHINE:X86 /DEF:pbvm125.def /OUT:pbvm125.lib
 lib /MACHINE:X86 /DEF:pbvm170.def /OUT:pbvm170.lib
+lib /MACHINE:X86 /DEF:pbvm190.def /OUT:pbvm190.lib
